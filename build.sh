63fc193706000b749f03e955580f30feef5507d3
Aldrin Abraham <aldrinabraham8@gmail.com>
2025-08-08 21:17:10 +0530

    Update for deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Force specific dependency versions
 pip install --upgrade pip
-pip install werkzeug==2.0.3 --no-deps
-pip install -r requirements.txt
+pip install -r requirements.txt --no-deps
+pip install click itsdangerous 
